378d9d006e07f6d69689d9cb625a5ae8f3561cdb
Suzie-0820 <85336611+Suzie-0820@users.noreply.github.com>
2021-06-04 09:40:01 +0200

    Add project files.

---
+++ Dice console/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Dice_console
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                int checkNumber, numberSides, numberRolls;
+
+                Console.WriteLine("How many sides do you want on the dice?");
+                string userAnswer = Console.ReadLine();
+                bool isSidesANumber = Int32.TryParse(userAnswer, out checkNumber);
+
+                while (!isSidesANumber)
+                {
+                    Console.WriteLine("This was not a valid number, please enter a new number");
+                    userAnswer = Console.ReadLine();
+                    isSidesANumber = Int32.TryParse(userAnswer, out checkNumber);
+                }
+                numberSides = checkNumber;
+
+                Console.WriteLine("How many times to do you want to roll the dice?");
+                userAnswer = Console.ReadLine();
+                bool isRollsANumber = Int32.TryParse(userAnswer, out checkNumber);
+
+                while (!isRollsANumber)
+                {
+                    Console.WriteLine("This was not a valid number, please enter a new number");
+                    userAnswer = Console.ReadLine();
+                    isRollsANumber = Int32.TryParse(userAnswer, out checkNumber);
+                }
+                numberRolls = checkNumber;
+
+                RollTheDice userDice = new RollTheDice(numberSides, numberRolls);
+                userDice.Results = userDice.RollingDice(userDice);
+                userDice.PrintOutResults(userDice);
+
+
+            }
+        }
+    }
+}
---
+++ Dice console/RollTheDice.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dice_console
+{
+    class RollTheDice
+    {
+        public int NumberSides { get; set; }
+        public int NumberRolls { get; set; }
+
+        public int[] Results { get; set; }
+
+        public RollTheDice (int a, int b)
+        {
+            NumberSides = a;
+            NumberRolls = b; 
+        }
+        
+        public int[] RollingDice(RollTheDice dice)
+        {
+            int[] sidesofDice = new int[dice.NumberSides];
+            for (int i =0; i<dice.NumberSides; i++)
+            {
+                sidesofDice[i] = 0; 
+            }
+
+            Random diceResult = new Random(); 
+
+            for (int i =0; i<dice.NumberRolls; i++)
+            {
+                int result = diceResult.Next(0, NumberSides);
+                sidesofDice[result] += 1;
+            }
+
+            return sidesofDice; 
+        }
+
+        public void PrintOutResults (RollTheDice dice)
+        {
+            int[] percentages = new int[dice.NumberSides];
+
+            for (int i = 0; i<dice.NumberSides; i++)
+            {
+                int percentage = (dice.Results[i]*100) / dice.NumberRolls;
+                percentages[i] = percentage;
+            }
+            
+            Console.WriteLine("Results of your rolls are: ");
+
+            for (int i =0; i<dice.Results.Length; i++)
+            {
+                Console.WriteLine($"Number {i + 1} was rolled {dice.Results[i]} times which is {percentages[i]} %"); 
+            }
+        }
+    }
+}
